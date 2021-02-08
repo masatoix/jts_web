0971018236c2c7d174d90ea813c27db2b005a191
Masato Ishida <masatoix@gmail.coma>
2021-02-08 13:52:46 +0900

    Comvert from jupyter to py file

--- 03_jts_data_web.py
+++ 03_jts_data_web.py
@@ -1,15 +1,16 @@
-# coding: UTF-8
-
+#%%
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+#%%
 input_file_cnd = 'output/condition/'
 input_file_phy = 'output/physical/'
 input_file_exe = 'output/exetra/'
 input_file_frq = 'output/frequency/'
 input_file_msr = 'output/measure/'
 
+#%%
 """
 # 次世代ターゲット事業
 ## `測定データベース`
@@ -28,6 +29,7 @@ ta_name = st.sidebar.selectbox("ターゲットアスリート名を選択", (
     "田中希湖",
     "石田美咲希"))
 
+#%%
 """
 ### 身長・体重
 """
@@ -50,6 +52,7 @@ if ta_name == ta_name:
             px.line(df, x='レポート月', y='体脂肪率', height=400)
         )
 
+#%%
 """
 ### コンディション
 """
@@ -102,6 +105,7 @@ if ta_name == ta_name:
             px.line(df, x='レポート月', y='ケガなど [足部の痛み]', height=400)
         )
 
+#%%
 """
 ### トレーニング頻度
 """
@@ -109,6 +113,7 @@ if ta_name == ta_name:
     df = pd.read_excel(input_file_exe+ta_name+'_training.xlsx')
     st.dataframe(df.style.highlight_max(axis=0), height=1000)
 
+#%%
 """
 ### トレーニング種目の頻度
 """
@@ -180,7 +185,8 @@ if ta_name == ta_name:
         st.write(
             px.line(df, x='レポート月', y='交代浴', height=400)
         )
-        
+
+#%%        
 """
 ### パフォーマンス測定
 """
@@ -215,6 +221,7 @@ if ta_name == ta_name:
             px.line(df, x='レポート月', y='20本ジャンプ (秒)', height=400)
         )
 
+#%%
 """
  
 ## `データ比較`
@@ -244,6 +251,7 @@ st.write(
         )
 )
 
+#%%
 """
 # 第57回全日本トランポリン競技選手権大会
 ---
@@ -256,143 +264,4 @@ st.write(df.style.highlight_max(), axis=0)
 st.write(
     px.scatter(df, x='D-score', y='E-score', size='Flight', color='age', log_x=True, size_max=15)
 )
-
-
-# ta_names = [
-#     "森ひかる",
-#     "高木裕美",
-#     "櫻井愛菜",
-#     "田中沙季",
-#     "増崎セリナ",
-#     "小野晴茄",
-#     "澤田守杏",
-#     "播磨ここね",
-#     "田中希湖",
-#     "石田美咲希",
-# ] 
-
-# def webdata_msr(df):
-#     st.write(
-#     px.bar(df, x='レポート月', y='立ち幅跳び ー 脚 (例: 2.1m)', height=400)
-#     )
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 全身 (例: 1.85m)', height=400)
-#     )
-#     st.write(
-#         px.line(df, x='レポート月', y='倒立 (秒)', height=400)
-#     )
-#     st.write(
-#         px.line(df, x='レポート月', y='閉眼片足 右 (秒) ', height=400)
-#     )
-#     st.write(
-#         px.line(df, x='レポート月', y='閉眼片足 (左)', height=400)
-#     )
-#     st.write(
-#         px.line(df, x='レポート月', y='20本ジャンプ (秒)', height=400)
-#     )
-
-# for g_name in ta_names:
-#     if　st.checkbox(g_name + 'のデータを表示'):
-#         df = pd.read_excel('output/measure/'+g_name+'_measure.xlsx')
-#         st.dataframe(df.style.highlight_max(axis=0), height=1000)
-#         webdata_msr(df)
-
-
-
-# """
-# # 石田美咲希
-# ## パフォーマンス測定
-# """
-
-# df = pd.read_excel('output/石田美咲希_measure_test.xlsx')
-# st.dataframe(df.style.highlight_max(axis=0), height=1000)
-
-# # des_list = df['month'].unique()
-
-# # selected_des = st.sidebar.selectbox(
-# #     '表示する種目を選択',
-# #     des_list
-# # )
-
-# # df = df[df['month']==selected_des]
-
-# if st.checkbox('グラフを表示'):
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 脚 (例: 2.1m)', height=400)
-#     )
-
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 全身 (例: 1.85m)', height=400)
-#     )
-
-#     st.write(
-#         px.line(df, x='レポート月', y='倒立 (秒)', height=400)
-#     )
-
-#     st.write(
-#         px.line(df, x='レポート月', y='閉眼片足 右 (秒) ', height=400)
-#     )
-
-#     st.write(
-#         px.line(df, x='レポート月', y='閉眼片足 (左)', height=400)
-#     )
-
-#     st.write(
-#         px.line(df, x='レポート月', y='20本ジャンプ (秒)', height=400)
-#     )
-
-# """
-# # 田中希湖
-# ## パフォーマンス測定
-# """
-
-# df = pd.read_excel('output/石田美咲希_measure_test.xlsx')
-# st.dataframe(df.style.highlight_max(axis=0), height=1000)
-
-# dataset_name = st.selectbox("Select Dataset", ("立ち幅跳びー脚", "立ち幅跳びー全身", "倒立", "20本ジャンプ"))
-# # st.write(dataset_name)
-
-# if dataset_name == '立ち幅跳びー脚':
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 脚 (例: 2.1m)', height=400)
-#     )
-# elif dataset_name == '立ち幅跳びー全身':
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 全身 (例: 1.85m)', height=400)
-#     )
-# elif dataset_name == '倒立':
-#     st.write(
-#         px.line(df, x='レポート月', y='倒立 (秒)', height=400)
-#     )
-
-# elif dataset_name == '20本ジャンプ':
-#     st.write(
-#         px.line(df, x='レポート月', y='20本ジャンプ (秒)', height=400)
-#     )
-
-# """
-# # 田中希湖
-# ## パフォーマンス測定
-# """
-
-# df = pd.read_excel('output/石田美咲希_measure_test.xlsx')
-# st.dataframe(df.style.highlight_max(axis=0), height=1000)
-
-
-
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 脚 (例: 2.1m)', height=400)
-#     )
-# elif dataset_name == '立ち幅跳びー全身':
-#     st.write(
-#         px.bar(df, x='レポート月', y='立ち幅跳び ー 全身 (例: 1.85m)', height=400)
-#     )
-# elif dataset_name == '倒立':
-#     st.write(
-#         px.line(df, x='レポート月', y='倒立 (秒)', height=400)
-#     )
-
-# elif dataset_name == '20本ジャンプ':
-#     st.write(
-#         px.line(df, x='レポート月', y='20本ジャンプ (秒)', height=400)
-    # )
+# %%
